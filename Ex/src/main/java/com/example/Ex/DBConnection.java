e3ae7aedf5cbc54df9a421d17fe5a8907861d349
andy <hei.andy.100@gmail.com>
2026-04-21 19:55:12 +0300

    feat: add DB connection and project structure

---
+++ Ex/src/main/java/com/example/Ex/DBConnection.java
@@ -0,0 +1,35 @@
+package com.example.Ex;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+
+
+    private String url = System.getenv("URL");
+    private String user = System.getenv("USER");
+    private String mdp = System.getenv("MDP");
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+
+    public Connection getDBConnection () throws SQLException {
+        return DriverManager.getConnection(url,user,mdp);
+
+    }
+
+
+}
