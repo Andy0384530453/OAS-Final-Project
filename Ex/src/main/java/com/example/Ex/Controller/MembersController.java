0ca15b56694a9e006319c510ab88b1e6b848f681
antso.tech <hei.antso.2007@gmail.com>
2026-04-22 07:11:38 +0300

    add collectivity and members controllers

--- Ex/src/main/java/com/example/Ex/Controller/MembersController.java
+++ Ex/src/main/java/com/example/Ex/Controller/MembersController.java
@@ -1,5 +1,19 @@
 package com.example.Ex.Controller;
 
+import java.lang.reflect.Member;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class MembersController {
-    
+
+    public ResponseEntity<?> memberRegistration(Member member){
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(null);
+
+            
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
